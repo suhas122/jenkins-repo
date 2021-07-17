1a39b39d52382a1d9cd32daaf882ab55ebc71134
root <root@localhost.localdomain>
2021-07-17 13:35:06 -0400

    post script modify

--- post-script.sh
+++ post-script.sh
@@ -10,7 +10,7 @@ branch="${REF[2]}"
 
 if [ $branch == "main" ]; then
 	crumb=$(curl -u "jenkins:1234" -s 'http://192.168.1.192:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
-	curl -u "jenkins:1234" -H "$crumb" -X POST http://192.168.1.192:8080/job/test-jenkins-job/build?delay=0sec
+	curl -u "jenkins:1234" -H "$crumb" -X POST http://192.168.1.192:8080/job/git-test-job/build?delay=0sec
 
 	if [ $? -eq 0 ]; then
 		echo "*** Ok"
